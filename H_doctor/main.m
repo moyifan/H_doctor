b197057606e056d748dcb10a22ef62bca0fb43ca
莫莫 <zhiren@zhirendembp.lan>
2017-12-06 13:30:52 +0800

    Initial Commit

---
+++ H_doctor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  H_doctor
+//
+//  Created by zhiren on 2017/12/6.
+//  Copyright © 2017年 zhiren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
